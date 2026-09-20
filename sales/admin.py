d9864a587b56884fece2617f2134422e92e80aec
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-20 18:51:16 +0200

    feat: register sales models in Django admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Customer, Product, Bill, Order, Producttype
 
 # Register your models here.
+
+#   Hiermit regestrieren wir alle Nutzer aus der Tabelle Customer im Admin 
+#   pannel 
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Bill)
+admin.site.register(Order)
+admin.site.register(Producttype)
